6543444de8d34db38187f25dd36cafde7df31186
Verbovenko <v.verbovenko@gmail.com>
2022-01-25 16:59:02 +0600

    Initial Commit

---
+++ Emassi/EmassiApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmassiApp.swift
+//  Emassi
+//
+//  Created by MacBook Pro on 25.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct EmassiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
